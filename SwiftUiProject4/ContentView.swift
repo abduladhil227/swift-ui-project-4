4f0add10e8213327d5cdd376e0c7ebad74b5fd1b
abduladhil227 <abduladhil006@gmail.com>
2024-08-20 22:35:58 +0530

    date and time picker in swift

--- SwiftUiProject4/ContentView.swift
+++ SwiftUiProject4/ContentView.swift
@@ -8,14 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var wakeUp = Date.now
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+       
+        //DatePicker("Please enter your date",selection: $wakeUp)
+        
+        // this date picker alway make a space for lable if we remove lable screen readers and voice over have no idea what this picker will do
+        
+        //DatePicker("",selection: $wakeUp)
+        
+        //To fix the problem hide the lable
+        DatePicker("Please enter your date",selection: $wakeUp)
+            .labelsHidden()
+        
+        // we can modify the display component
+        DatePicker("Please enter your date",selection: $wakeUp,displayedComponents: .hourAndMinute)
+            .labelsHidden()
+        
+        // one side range in swift date, it will allow us to pick date from future not from past
+        DatePicker("Please enter your date",selection: $wakeUp,in: Date.now...)
+            .labelsHidden()
+        
+    }
+    //We can set range in Date also
+    func exampleDates(){
+        let tomorrow = Date.now.addingTimeInterval(86400) //86400 one day in seconds
+        let range =  Date.now...tomorrow
     }
 }
 
