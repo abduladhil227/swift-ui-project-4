bb9805ece22f09978c0f69d245c1502c72401420
abduladhil227 <abduladhil006@gmail.com>
2024-08-20 22:05:50 +0530

    Initial Commit

---
+++ SwiftUiProject4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUiProject4
+//
+//  Created by Abdul Adhil on 20/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUiProject4/SwiftUiProject4App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiProject4App.swift
+//  SwiftUiProject4
+//
+//  Created by Abdul Adhil on 20/08/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiProject4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
